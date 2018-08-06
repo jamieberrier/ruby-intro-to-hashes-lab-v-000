a8cf8972621b69758c4bcf12d1526e1cd7128bec
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 00:29:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -61,11 +61,14 @@ def monopoly_with_fourth_tier
 			pieces: 4,
 			names:
 			{
-				reading_railroad: 
+				reading_railroad:
 				{
 					"mortgage_value" = "$100"
 				},
-				pennsylvania_railroad: {},
+				pennsylvania_railroad: 
+				{
+					"" = ""
+				},
 				b_and_o_railroad: {},
 				shortline: {}
 			},
