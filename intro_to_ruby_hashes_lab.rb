9238558e7a586dad5e91a3f5ee31b01b3873083d
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 00:29:50 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -69,11 +69,14 @@ def monopoly_with_fourth_tier
 				{
 					"mortgage_value" = "$200"
 				},
-				b_and_o_railroad: 
+				b_and_o_railroad:
 				{
 					"mortgage_value" = "$400"
 				},
-				shortline: {}
+				shortline: 
+				{
+					"mortgage_value" = "$800"
+				}
 			},
 			rent_in_dollars:
 			{
